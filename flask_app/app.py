c781839af453e1e45608bb0a1752796a665f3557
meowoodie <woodielove@163.com>
2015-06-09 17:33:07 +0800

    add logentries

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,8 +2,10 @@
 ## This requires the 'blinker' package to be installed
 from flask import Flask, request
 from flask import got_request_exception
+from poi_analyser_lib.settings import *
 from poi_analyser_lib.trainer import Trainer
 from poi_analyser_lib.predictor import Predictor
+from poi_analyser_lib.logger import log
 import os
 import rollbar
 import rollbar.contrib.flask
@@ -16,9 +18,9 @@ def init_rollbar():
     """init rollbar module"""
     rollbar.init(
         # access token for the demo app: https://rollbar.com/demo
-        '5880ff452a72481fb3af605801652a63',
+        ROLLBAR_TOKEN,
         # environment name
-        'production',
+        "production",
         # server root directory, makes tracebacks prettier
         root=os.path.dirname(os.path.realpath(__file__)),
         # flask already sets up logging
@@ -27,35 +29,56 @@ def init_rollbar():
     # send exceptions from `app` to rollbar, using flask's signal system.
     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
-@app.route("/trainingGMM/", methods=["POST"])
+@app.route("/trainingSpecificPOI/", methods=["POST"])
 def trainingPOI():
+    train_tag = "[Training Process] "
+
     # Analyse the incoming data.
     data = json.loads(request.data)
     print "Received data is", data
+    log.info(train_tag + "Received request data.")
 
     _obs = data["obs"]
     _model = data["model"]
+    log.debug(train_tag + "Received observation is: " + _obs + " and model is: " + _model)
     # _location_type = data["config"]["locationType"]
 
     t = Trainer(_model)
     t.fit(_obs)
+    log.info(train_tag + "Fitting is complete.")
+    log.debug(train_tag + "Fitting model is:" + t.modelParams())
 
     result = json.dumps({"result": t.modelParams(), "code": 0, "message": "Training successfully"})
     return result
 
 @app.route("/predictPoi/", methods=["POST"])
 def predictPOI():
+    predict_tag = "[Predicting Process] "
+
     # Analyse the incoming data.
     data = json.loads(request.data)
     print "Received data is", data
+    log.info(predict_tag + "Received request data.")
+
     _t = data["seq"]
     _models = data["models"]
+    log.debug(predict_tag + "Received sequence is: " + _t + " and models are: " + _models)
 
     p = Predictor(_models)
     scores = p.scores(_t)
+    log.info(predict_tag + "Predicting is complete.")
+    log.debug(predict_tag + "Prediction is: " + scores)
 
     result = json.dumps({"result": scores, "code": 0, "message": "Predicting successfully"})
     return result
+
+# @app.route('/')
+# def hello():
+#     print "in hello"
+#     log.info('Im an info message')
+#     x = None
+#     x[5]
+#     return "Hello World!"
 
 
 if __name__ == "__main__":
