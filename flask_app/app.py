6bdb3f2f8e530a36d58ca2fd1f2e3376f96e5830
meowoodie <woodielove@163.com>
2015-06-09 16:28:35 +0800

    add rollbar

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,16 +1,31 @@
-# -*- coding: UTF-8 -*-
-
-import numpy as np
-__author__ = "MeoWoodie"
-
-from flask import Flask, request, url_for, Response, send_file
-from poi_analyser_lib.datasets import Dataset
+## Rollbar init code. You'll need the following to use Rollbar with Flask.
+## This requires the 'blinker' package to be installed
+from flask import Flask, request
+from flask import got_request_exception
 from poi_analyser_lib.trainer import Trainer
 from poi_analyser_lib.predictor import Predictor
+import os
+import rollbar
+import rollbar.contrib.flask
 import json
 
-
 app = Flask(__name__)
+
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token for the demo app: https://rollbar.com/demo
+        '5880ff452a72481fb3af605801652a63',
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 @app.route("/trainingGMM/", methods=["POST"])
 def trainingPOI():
@@ -44,5 +59,5 @@ def predictPOI():
 
 
 if __name__ == "__main__":
-    app.debug = True
+    # app.debug = True
     app.run(host="0.0.0.0", port=9010)
