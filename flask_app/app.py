745ab59ce1ad1b1cad4fce70466d517df5e472e9
meowoodie <woodielove@163.com>
2015-06-09 21:12:36 +0800

    add logentries and rollbar

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,14 +1,14 @@
 # -*- coding: UTF-8 -*-
-## Rollbar init code. You'll need the following to use Rollbar with Flask.
-## This requires the 'blinker' package to be installed
+## Rollbar init code. You"ll need the following to use Rollbar with Flask.
+## This requires the "blinker" package to be installed
 from flask import Flask, request, got_request_exception
 from poi_analyser_lib.settings import *
 from poi_analyser_lib.trainer import Trainer
 from poi_analyser_lib.predictor import Predictor
-# from poi_analyser_lib.logger import log
-# import os
-# import rollbar
-# import rollbar.contrib.flask
+from poi_analyser_lib.logger import log
+import os
+import rollbar
+import rollbar.contrib.flask
 import json
 
 app = Flask(__name__)
@@ -26,57 +26,97 @@ app = Flask(__name__)
 #         # flask already sets up logging
 #         allow_logging_basic_config=False)
 #
-#     # send exceptions from `app` to rollbar, using flask's signal system.
+#     # send exceptions from `app` to rollbar, using flask"s signal system.
 #     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 @app.route("/trainingSpecificPOI/", methods=["POST"])
 def trainingPOI():
     train_tag = "[Training Process] "
+    result = {"code": 1, "message": ""}
 
-    # Analyse the incoming data.
-    data = json.loads(request.data)
-    print "Received data is", data
-    # log.info(train_tag + "Received request data.")
+    try:
+        # Analyse the incoming data.
+        data = json.loads(request.data)
+        print "Received data is", data
+    except ValueError, err_msg:
 
-    _obs = data["obs"]
-    _model = data["model"]
-    # log.debug(train_tag + "Received observation is: " + _obs + " and model is: " + _model)
-    # _location_type = data["config"]["locationType"]
+        log.error(train_tag + " ValueError: %s, params=%s" % (err_msg, request.data))
+
+        result["message"] = "Unvalid params: NOT a JSON Object"
+        return json.dumps(result)
+
+    log.info(train_tag + "Received request data")
+
+    try:
+        _obs = data["obs"]
+        _model = data["model"]
+    except KeyError, key:
+
+        log.error(train_tag + " KeyError: There is no key named %s, params=%s" % (key, request.data))
+
+        result["message"] = "Unvalid params: There is NO key named %s" % key
+        return json.dumps(result)
+
+    log.debug(train_tag + "Received observation is: %s and model is: %s" % (_obs, _model))
 
     t = Trainer(_model)
     t.fit(_obs)
-    # log.info(train_tag + "Fitting is complete.")
-    # log.debug(train_tag + "Fitting model is:" + t.modelParams())
 
-    result = json.dumps({"result": t.modelParams(), "code": 0, "message": "Training successfully"})
-    return result
+    log.info(train_tag + "Fitting is complete")
+    log.debug(train_tag + "Fitting model is: %s" % t.modelParams())
+
+    result["code"]    = 0
+    result["result"]  = t.modelParams()
+    result["message"] = "Training successfully"
+
+    return json.dumps(result)
 
 @app.route("/predictPoi/", methods=["POST"])
 def predictPOI():
     predict_tag = "[Predicting Process] "
+    result = {"code": 1, "message": ""}
 
-    # Analyse the incoming data.
-    data = json.loads(request.data)
-    print "Received data is", data
-    # log.info(predict_tag + "Received request data.")
-    print "cao!"
-    _t = data["seq"]
-    _models = data["models"]
-    # log.debug(predict_tag + "Received sequence is: " + _t + " and models are: " + _models)
+    try:
+        # Analyse the incoming data.
+        data = json.loads(request.data)
+        print "Received data is", data
+    except ValueError, err_msg:
+
+        log.error(predict_tag + " ValueError: %s, params=%s" % (err_msg, request.data))
+
+        result["message"] = "Unvalid params: NOT a JSON Object"
+        return json.dumps(result)
+
+    log.info(predict_tag + "Received request data")
+
+    try:
+        _t = data["seq"]
+        _models = data["models"]
+    except KeyError, key:
+
+        log.error(predict_tag + " KeyError: There is no key named %s, params=%s" % (key, request.data))
+
+        result["message"] = "Unvalid params: There is NO key named %s" % key
+        return json.dumps(result)
+
+    log.debug(predict_tag + "Received sequence is: %s and models are: %s" % (_t, _models))
 
     p = Predictor(_models)
     scores = p.scores(_t)
-    # log.info(predict_tag + "Predicting is complete.")
-    # log.debug(predict_tag + "Prediction is: " + scores)
 
-    result = json.dumps({"result": scores, "code": 0, "message": "Predicting successfully"})
-    return result
+    log.info(predict_tag + "Predicting is complete")
+    log.debug(predict_tag + "Prediction is: %s" % scores)
 
-# @app.route('/', methods=["GET", "POST"])
+    result["code"]    = 0
+    result["result"]  = scores
+    result["message"] = "Predicting successfully"
+    return json.dumps(result)
+
+# @app.route("/", methods=["GET", "POST"])
 # def hello():
 #     print "in hello"
 #     papapa = json.loads("{}")
-#     log.info('Im an info message')
+#     log.info("Im an info message")
 #     log.debug("papapa")
 #     x = None
 #     x[5]
--- flask_app/poi_analyser_lib/logger.py
+++ flask_app/poi_analyser_lib/logger.py
@@ -1,8 +1,8 @@
-# from logentries import LogentriesHandler
-# from settings import *
-# import logging
-#
-# log = logging.getLogger('rollbar')
-# log.setLevel(logging.INFO)
-# # Note if you have set up the logentries handler in Django, the following line is not necessary
-# log.addHandler(LogentriesHandler(LOGENTRIES_TOKEN))
+from logentries import LogentriesHandler
+from settings import *
+import logging
+
+log = logging.getLogger('logentries')
+log.setLevel(logging.INFO)
+# Note if you have set up the logentries handler in Django, the following line is not necessary
+log.addHandler(LogentriesHandler(LOGENTRIES_TOKEN))
