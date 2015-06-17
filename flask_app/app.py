78cdc7e3653d884885abf1acd670dd02b7983662
meowoodie <woodielove@163.com>
2015-06-17 23:22:27 +0800

    test bugsnag

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-# import bugsnag
+import bugsnag
 import os
 import json
 import datetime
@@ -7,23 +7,22 @@ from poi_analyser_lib.config import *
 from poi_analyser_lib.trainer import Trainer
 from poi_analyser_lib.predictor import Predictor
 from poi_analyser_lib.logger import log
-# from bugsnag.flask import handle_exceptions
+from bugsnag.flask import handle_exceptions
 
+# Configure Bugsnag
+bugsnag.configure(
+    api_key=BUGSNAG_TOKEN,
+    project_root=os.path.dirname(os.path.realpath(__file__)),
+)
 app = Flask(__name__)
+# Attach Bugsnag to Flask's exception handler
+handle_exceptions(app)
 
 @app.before_first_request
 def init_before_first_request():
     import datetime
 
     init_tag = "[Initiation of Service Process]\n"
-
-    # Configure Bugsnag
-    # bugsnag.configure(
-    #     api_key=BUGSNAG_TOKEN,
-    #     project_root=os.path.dirname(os.path.realpath(__file__)),
-    # )
-    # Attach Bugsnag to Flask's exception handler
-    # handle_exceptions(app)
 
     log_init_time = "Initiation START at: \t%s\n" % datetime.datetime.now()
     log_app_env = "Environment Variable: \t%s\n" % APP_ENV
